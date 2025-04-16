8b2b433087c6071b97fec32a57fb5de1838f2e35
Zachary Knowlan <zachary.knowlan@gmail.com>
2025-04-16 17:31:20 -0400

    Removed openmpi and substituted g++. Also fixed Kokkos install directory

--- ActionsInstall.sh
+++ ActionsInstall.sh
@@ -8,16 +8,15 @@ export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:../Catch2/build/install
 git clone https://github.com/kokkos/kokkos.git ../Kokkos
 cmake -S ../Kokkos -B ../Kokkos/build/ \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_CXX_COMPILER=`which mpicxx` \
-  -DCMAKE_INSTALL_PREFIX=../Catch2/build/install/ \
-  -DKokkos_ENABLE_OPENMP=ON \
-  -DKokkos_ENABLE_CUDA=ON 
+  -DCMAKE_CXX_COMPILER=`which g++` \
+  -DCMAKE_INSTALL_PREFIX=../Kokkos/build/install/ \
+  -DKokkos_ENABLE_OPENMP=ON
 cmake --build ../Kokkos/build/ -j 8 --target install
 
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:../Kokkos/build/install
 
 cmake -S . -B build \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_CXX_EXTENSIONS=OFF \
-  -DCMAKE_CXX_COMPILER=`which mpicxx`
+  -DCMAKE_CXX_COMPILER=`which g++` \
+  -DAssignment_ENABLE_CUDA=OFF
 cmake --build build -j 8
